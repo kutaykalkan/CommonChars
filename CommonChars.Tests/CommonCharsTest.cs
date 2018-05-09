48f24525c4ba3053dc6e7a93721624582731ef22
kutaykalkan <kutaykalkan@hotmail.com>
2018-05-09 15:22:48 -0400

    Initial commit

---
+++ CommonChars.Tests/CommonCharsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using NUnit.Framework;
+
+namespace CommonChars.Tests
+{
+    [TestFixture]
+    public class CommonCharsTest
+    {
+        [TestCase("kutay", "kalkan", ExpectedResult = "ka")]
+        [TestCase("aaa", "bbb", ExpectedResult = "")]
+        public string Test(string a, string b)
+        {
+            CommonChars commonChars = new CommonChars();
+            return commonChars.FindCommonChars(a, b);
+        }
+
+        [Test]
+        public void ThrowExceptionWhenNullInput()
+        {
+            CommonChars commonChars = new CommonChars();
+            Assert.Throws<ArgumentNullException>(() => commonChars.FindCommonChars(null, null),
+                "Input strings can not be null");
+        }
+    }
+}
---
+++ CommonChars/CommonChars.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommonChars
+{
+    /// <summary>
+    /// Time optimized function to find common chars btw 2 strings.
+    /// </summary>
+    public class CommonChars
+    {
+        public string FindCommonChars(string a, string b)
+        {
+            if (a == null || b == null)
+            {
+                throw new ArgumentNullException(@"Input strings can not be null");
+            }
+            HashSet<char> chars = new HashSet<char>();
+            HashSet<char> result = new HashSet<char>();
+            foreach (var c in a)
+            {
+                chars.Add(c);
+            }
+
+            foreach (var c in b)
+            {
+                if (chars.Contains(c))
+                    result.Add(c);
+            }
+            return String.Join("", result);
+        }
+    }
+}
